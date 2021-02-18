6479492c352aba4c9670baf9d4494cdfc5cc82d9
Andrii Pelesh <andrii.pelesh@gmail.com>
2021-02-18 07:23:13 -0800

    Define dbContext

---
+++ Transformers.Model/Enums/Allegiance.cs
@@ -0,0 +1,9 @@
+namespace Transformers.Model.Enums
+{
+    public enum Allegiance
+    {
+        Undefined = 0,
+        Autobot = 1,
+        Decepticon = 2
+    }
+}
---
+++ Transformers.Model/IDbContext.cs
@@ -0,0 +1,19 @@
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Transformers.Model.Entities;
+using Transformers.Model.Enums;
+
+namespace Transformers.Model
+{
+    public interface IDbContext
+    {
+        public DbSet<Transformer> Transformers { get; }
+
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+
+        IQueryable<Transformer> GetAllAutobots() => Transformers.Where(t => t.Allegiance == Allegiance.Autobot);
+        IQueryable<Transformer> GetAllDecepticon() => Transformers.Where(t => t.Allegiance == Allegiance.Autobot);
+    }
+}
